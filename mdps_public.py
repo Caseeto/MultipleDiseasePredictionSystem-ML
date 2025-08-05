87066e61177f29b3b0c311a221494afb5cec127e
Ashish Saranshi <92679039+Caseeto@users.noreply.github.com>
2025-08-05 22:15:21 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -338,21 +338,22 @@ if (selected == 'Breast Cancer Prediction'):
     
 
 
-# code for Prediction
-bc_diagnosis = ''
+	# code for Prediction
+	bc_diagnosis = ''
 
 
-# creating a button for Prediction    
-if st.button("Parkinson's Test Result"):
-    bc_prediction = breast_cancer_model.predict([[radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst]])                          
+	# creating a button for Prediction    
+	if st.button("Parkinson's Test Result"):
+    		bc_prediction = breast_cancer_model.predict([[radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst]])                          
     
-    if (bc_prediction[0] == 0):
-      bc_diagnosis = "The Breast cancer is Malignant"
-    else:
-      bc_diagnosis = "The Breast Cancer is Benign"
+    		if (bc_prediction[0] == 0):
+      			bc_diagnosis = "The Breast cancer is Malignant"
+    		else:
+      			bc_diagnosis = "The Breast Cancer is Benign"
     
 
-st.success(bc_diagnosis)
+	st.success(bc_diagnosis)
+
 
 
 
